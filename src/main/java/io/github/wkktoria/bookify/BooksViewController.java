771e391368d13ba7ae6fbf419b15d10facc7d81c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-03-04 21:09:52 +0100

    Create simple books view

--- src/main/java/io/github/wkktoria/bookify/BooksViewController.java
+++ src/main/java/io/github/wkktoria/bookify/BooksViewController.java
@@ -1,14 +1,34 @@
 package io.github.wkktoria.bookify;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class BooksViewController {
 
+    private static final Map<Integer, String> database = new HashMap<>();
+
+    static {
+        database.put(1, "Clean Code: A Handbook of Agile Software Craftsmanship");
+        database.put(2, "Refactoring: Improving the Design of Existing Code");
+        database.put(3, "Fundamentals of Software Architecture: A Modern Engineering Approach");
+        database.put(4, "Effective Java");
+        database.put(5, "Spring Start Here: Learn what you need and learn it well");
+    }
+
     @GetMapping("/")
     public String home() {
-        return "home.html";
+        return "home";
+    }
+
+    @GetMapping("/view/books")
+    public String books(Model model) {
+        model.addAttribute("bookMap", database);
+        return "books";
     }
 
 }
