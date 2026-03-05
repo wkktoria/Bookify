55aa64e6b2949983ca5072d8338990855c644247
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-03-05 14:51:45 +0100

    Change field name

--- src/main/java/io/github/wkktoria/bookify/book/dto/response/PartiallyUpdateBookResponseDto.java
+++ src/main/java/io/github/wkktoria/bookify/book/dto/response/PartiallyUpdateBookResponseDto.java
@@ -2,5 +2,5 @@ package io.github.wkktoria.bookify.book.dto.response;
 
 import io.github.wkktoria.bookify.book.controller.Book;
 
-public record PartiallyUpdateBookResponseDto(Book book) {
+public record PartiallyUpdateBookResponseDto(Book updatedBook) {
 }
