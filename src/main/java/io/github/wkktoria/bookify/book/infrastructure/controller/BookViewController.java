9c4b9c7b52b07898ad6dccd5c419a79c1717e804
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-03-13 13:06:44 +0100

    Use service in view controller

--- src/main/java/io/github/wkktoria/bookify/book/infrastructure/controller/BookViewController.java
+++ src/main/java/io/github/wkktoria/bookify/book/infrastructure/controller/BookViewController.java
@@ -1,24 +1,17 @@
 package io.github.wkktoria.bookify.book.infrastructure.controller;
 
-import io.github.wkktoria.bookify.book.domain.model.Book;
+import io.github.wkktoria.bookify.book.domain.service.BookRetriever;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class BookViewController {
 
-    private static final Map<Integer, Book> database = new HashMap<>();
+    private final BookRetriever bookRetriever;
 
-    static {
-        database.put(1, new Book("Clean Code: A Handbook of Agile Software Craftsmanship", "Robert C. Martin"));
-        database.put(2, new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler"));
-        database.put(3, new Book("Fundamentals of Software Architecture: A Modern Engineering Approach", "Mark Richards & Neal Ford"));
-        database.put(4, new Book("Effective Java", "Joshua Bloch"));
-        database.put(5, new Book("Spring Start Here: Learn what you need and learn it well", "Laurentiu Spilca"));
+    BookViewController(BookRetriever bookRetriever) {
+        this.bookRetriever = bookRetriever;
     }
 
     @GetMapping("/")
@@ -28,7 +21,7 @@ public class BookViewController {
 
     @GetMapping("/view/books")
     public String books(Model model) {
-        model.addAttribute("bookMap", database);
+        model.addAttribute("bookList", bookRetriever.findAll());
         return "books";
     }
 
