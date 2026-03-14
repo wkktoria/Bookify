44124afdddae147d3352b3a5b6e8d03eadb403b6
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-03-14 22:03:38 +0100

    Add Flyway

--- src/main/resources/db/migration/V1__create_book_table.sql
+++ src/main/resources/db/migration/V1__create_book_table.sql
@@ -1,11 +1,10 @@
 CREATE TABLE book
 (
-    id               BIGINT                      NOT NULL,
+    id               BIGSERIAL PRIMARY KEY       NOT NULL,
     title            VARCHAR(255)                NOT NULL,
     author           VARCHAR(100)                NOT NULL,
     publication_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     isbn             VARCHAR(13)                 NOT NULL,
     pages            INTEGER                     NOT NULL,
-    language         VARCHAR(255),
-    CONSTRAINT pk_book PRIMARY KEY (id)
+    language         VARCHAR(255)
 );
