fabaec97007735ba0fc8a6e4cf28fdb06aa20255
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-03-18 12:58:08 +0100

    Insert sample data to database

---
+++ src/main/resources/db/migration/V13__insert_series.sql
@@ -0,0 +1,5 @@
+INSERT INTO series (id, name, uuid, created_on)
+VALUES (1, 'Clean Code Series', gen_random_uuid(), now()),
+       (2, 'DevOps Collection', gen_random_uuid(), now()),
+       (3, 'Java Mastery Series', gen_random_uuid(), now()),
+       (4, 'Algorithms Series', gen_random_uuid(), now());
---
+++ src/main/resources/db/migration/V14__insert_authors.sql
@@ -0,0 +1,13 @@
+INSERT INTO author (id, firstname, lastname, uuid, created_on)
+VALUES (1, 'Robert', 'Martin', gen_random_uuid(), now()),
+       (2, 'Martin', 'Fowler', gen_random_uuid(), now()),
+       (3, 'Joshua', 'Bloch', gen_random_uuid(), now()),
+       (4, 'Brian', 'Goetz', gen_random_uuid(), now()),
+       (5, 'Kathy', 'Sierra', gen_random_uuid(), now()),
+       (6, 'Andrew', 'Hunt', gen_random_uuid(), now()),
+       (7, 'David', 'Thomas', gen_random_uuid(), now()),
+       (8, 'Thomas', 'Cormen', gen_random_uuid(), now()),
+       (9, 'Robert', 'Sedgewick', gen_random_uuid(), now()),
+       (10, 'Gene', 'Kim', gen_random_uuid(), now()),
+       (11, 'Martin', 'Kleppmann', gen_random_uuid(), now()),
+       (12, 'Eric', 'Evans', gen_random_uuid(), now());
