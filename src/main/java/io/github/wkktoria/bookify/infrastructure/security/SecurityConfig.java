7b87706a25729745a5c009309eb91e7a9dd4bd9b
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-04 12:46:39 +0200

    Configure security filter chain

--- src/main/java/io/github/wkktoria/bookify/infrastructure/security/SecurityConfig.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/security/SecurityConfig.java
@@ -4,9 +4,13 @@ import io.github.wkktoria.bookify.domain.usercrud.UserRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Log4j2
@@ -20,6 +24,14 @@ class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
 }
