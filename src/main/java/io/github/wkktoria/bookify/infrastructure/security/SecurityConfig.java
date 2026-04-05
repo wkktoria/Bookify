c87a3c4a9a2849c347187a0e9d9562349c717417
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-05 22:00:22 +0200

    Delegate login to manager

--- src/main/java/io/github/wkktoria/bookify/infrastructure/security/SecurityConfig.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/security/SecurityConfig.java
@@ -5,7 +5,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
@@ -22,6 +24,12 @@ import java.util.List;
 @Configuration
 @Log4j2
 class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public UserDetailsManager userDetailsService(final UserRepository userRepository) {
--- src/main/java/io/github/wkktoria/bookify/infrastructure/security/jwt/JwtGenerator.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/security/jwt/JwtGenerator.java
@@ -1,11 +1,24 @@
 package io.github.wkktoria.bookify.infrastructure.security.jwt;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Log4j2
 class JwtGenerator {
 
+    private final AuthenticationManager authenticationManager;
+
     String authenticateAndGenerateToken(final String username, final String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                username, password
+        );
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
         return "token123";
     }
 
