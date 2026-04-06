18426800d68583864a12f4bb467d461212f65fa5
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-06 18:15:35 +0200

    Use HMAC256 algorithm in token generation

--- src/main/java/io/github/wkktoria/bookify/infrastructure/security/jwt/JwtGenerator.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/security/jwt/JwtGenerator.java
@@ -1,6 +1,7 @@
 package io.github.wkktoria.bookify.infrastructure.security.jwt;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import io.github.wkktoria.bookify.infrastructure.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,6 +21,8 @@ import java.time.ZoneOffset;
 @Log4j2
 class JwtGenerator {
 
+    public static final String ROLES_CLAIM_NAME = "roles";
+
     private final AuthenticationManager authenticationManager;
     private final Clock clock;
     private final JwtConfigurationProperties properties;
@@ -32,13 +35,14 @@ class JwtGenerator {
         SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
         Instant issuedAt = LocalDateTime.now(clock).toInstant(ZoneOffset.UTC);
         Instant expiresAt = issuedAt.plus(Duration.ofMinutes(properties.expirationMinutes()));
+        Algorithm algorithm = Algorithm.HMAC256(properties.secret());
         return JWT.create()
                 .withSubject(securityUser.getUsername())
                 .withIssuedAt(issuedAt)
                 .withExpiresAt(expiresAt)
                 .withIssuer(properties.issuer())
-                .withClaim("roles", securityUser.getAuthoritiesAsString())
-                .sign(null);
+                .withClaim(ROLES_CLAIM_NAME, securityUser.getAuthoritiesAsString())
+                .sign(algorithm);
     }
 
 }
