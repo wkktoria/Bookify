133dfc4ee1128165974291480cb5aac553e7d120
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-13 20:25:33 +0200

    Update register request DTO
    Username should be an email.

--- src/main/java/io/github/wkktoria/bookify/infrastructure/usercrud/controller/RegisterUserRequestDto.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/usercrud/controller/RegisterUserRequestDto.java
@@ -1,8 +1,10 @@
 package io.github.wkktoria.bookify.infrastructure.usercrud.controller;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 record RegisterUserRequestDto(
+        @Email
         @NotBlank
         String username,
 
