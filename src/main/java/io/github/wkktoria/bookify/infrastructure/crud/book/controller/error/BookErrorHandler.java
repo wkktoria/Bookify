5fdef219a52c166e3ac2a84f1c373eb61c7eb073
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-24 13:41:50 +0200

    Add integration test for book error handler

--- src/main/java/io/github/wkktoria/bookify/infrastructure/crud/book/controller/error/BookErrorHandler.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/crud/book/controller/error/BookErrorHandler.java
@@ -1,6 +1,7 @@
 package io.github.wkktoria.bookify.infrastructure.crud.book.controller.error;
 
 import io.github.wkktoria.bookify.domain.crud.BookNotFoundException;
+import io.github.wkktoria.bookify.infrastructure.crud.book.controller.BookRestController;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,14 +9,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
+@ControllerAdvice(assignableTypes = BookRestController.class)
 @Log4j2
 public class BookErrorHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorBookResponseDto handleException(BookNotFoundException exception) {
+    public ErrorBookResponseDto handleBookNotFoundException(final BookNotFoundException exception) {
         log.warn("Book not found: {}", exception.getMessage());
         return new ErrorBookResponseDto(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
