8774bc9b6f3aab8e855126e800b3590ea65e854e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-24 13:54:41 +0200

    Implement author error handler

--- src/main/java/io/github/wkktoria/bookify/infrastructure/crud/author/controller/error/AuthorErrorHandler.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/crud/author/controller/error/AuthorErrorHandler.java
@@ -1,4 +1,24 @@
 package io.github.wkktoria.bookify.infrastructure.crud.author.controller.error;
 
+import io.github.wkktoria.bookify.domain.crud.AuthorNotFoundException;
+import io.github.wkktoria.bookify.infrastructure.crud.author.controller.AuthorRestController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice(assignableTypes = AuthorRestController.class)
+@Log4j2
 class AuthorErrorHandler {
+
+    @ExceptionHandler(AuthorNotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    ErrorAuthorResponseDto handleAuthorNotFoundException(final AuthorNotFoundException exception) {
+        log.warn("Author not found: {}", exception.getMessage());
+        return new ErrorAuthorResponseDto(exception.getMessage());
+    }
+
 }
