1411fcc01418da8b2076d2de4ca093c8d813d12e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-26 22:10:29 +0200

    Handle genre not deleted exception

--- src/main/java/io/github/wkktoria/bookify/domain/crud/GenreNotDeletedException.java
+++ src/main/java/io/github/wkktoria/bookify/domain/crud/GenreNotDeletedException.java
@@ -1,6 +1,6 @@
 package io.github.wkktoria.bookify.domain.crud;
 
-class GenreNotDeletedException extends RuntimeException {
+public class GenreNotDeletedException extends RuntimeException {
 
     public GenreNotDeletedException(final String message) {
         super(message);
