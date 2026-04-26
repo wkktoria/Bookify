c15f2918773b5345bf82a362339b53b23ef12117
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-26 23:11:01 +0200

    Add update series method to controller

---
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/crud/series/controller/dto/request/UpdateSeriesRequestDto.java
@@ -0,0 +1,10 @@
+package io.github.wkktoria.bookify.infrastructure.crud.series.controller.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateSeriesRequestDto(
+        @NotNull(message = "name must not be null")
+        @NotEmpty(message = "name must not be empty")
+        String name) {
+}
