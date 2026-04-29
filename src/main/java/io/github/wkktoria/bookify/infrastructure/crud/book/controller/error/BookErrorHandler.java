98de45364480db8d86d01095385dbcb7044e4da3
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-29 14:59:38 +0200

    Document series API in Swagger

--- src/main/java/io/github/wkktoria/bookify/infrastructure/crud/book/controller/error/BookErrorHandler.java
+++ src/main/java/io/github/wkktoria/bookify/infrastructure/crud/book/controller/error/BookErrorHandler.java
@@ -4,6 +4,7 @@ import io.github.wkktoria.bookify.domain.crud.BookNotFoundException;
 import io.github.wkktoria.bookify.infrastructure.crud.author.controller.AuthorRestController;
 import io.github.wkktoria.bookify.infrastructure.crud.book.controller.BookRestController;
 import io.github.wkktoria.bookify.infrastructure.crud.dto.ErrorResponseDto;
+import io.github.wkktoria.bookify.infrastructure.crud.series.controller.SeriesRestController;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice(assignableTypes = {BookRestController.class, AuthorRestController.class})
+@ControllerAdvice(assignableTypes = {BookRestController.class, AuthorRestController.class, SeriesRestController.class})
 @Log4j2
 public class BookErrorHandler {
 
