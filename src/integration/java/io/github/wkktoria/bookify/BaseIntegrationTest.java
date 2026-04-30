75a6d346009a6f3a9ca32079aa593a8cd8c15dd8
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-30 16:28:24 +0200

    Create test container per class to avoid problems with running all tests at once

--- src/integration/java/io/github/wkktoria/bookify/BaseIntegrationTest.java
+++ src/integration/java/io/github/wkktoria/bookify/BaseIntegrationTest.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(classes = BookifyApplication.class)
@@ -16,18 +13,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @ActiveProfiles("integration")
 public class BaseIntegrationTest {
-
-    public final static PostgreSQLContainer<?> postgreSQLContainer;
-
-    static {
-        postgreSQLContainer = new PostgreSQLContainer<>("postgres:18-alpine");
-        postgreSQLContainer.start();
-    }
-
-    @DynamicPropertySource
-    public static void propertyOverride(final DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-    }
 
     @Autowired
     public MockMvc mockMvc;
